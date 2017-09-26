2895598354bc909fd2a356a9e3bceacaf968bbce
yasminjonson <yasminj2019@gmail.com>
2017-09-26 12:38:43 -0400

    player number bullshit

--- TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/TriviaMania.cs
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/TriviaMania.cs
@@ -17,15 +17,83 @@ namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
         
         public override void play()
         {
-            // code here
+            // CODE HERE
 
             // print name and promp user for name and available prizes
+            writeLine(" Hello and welcome to Trivia Mania!!!" );
+            writeLine(" Get five questions in a row right and win the ~Grand Prize~ ");
+            writeLine(" How many players will be participating this round?  " );
 
-            // numer of players
-            Console.WriteLine("Hello and welcome to Trivia Mania!!!" +
-                "Please select number of players: ");
+            // show choices of number of players 
+            writeLine(" [1] Single Player");
+            writeLine(" [2] Player v. Player ");
+            writeLine(" [3] Triple Threat ");
+            writeLine(" [4] Quadruple Showdown ");
 
+            String playerNum = getInput();
 
+            // number of players
+
+                if (playerNum == "1")
+                {
+
+                 // prompt names
+                 writeLine("What's your name?");
+                 String p1 = getInput();
+
+                }
+                else if (playerNum == "2")
+                {
+
+                // prompt names
+                 writeLine("What're your names?");
+
+                 writeLine("Player 1: ");
+                 String p1 = getInput();
+
+                 writeLine("Player 2: ");
+                 String p2 = getInput();
+
+                }
+
+                else if (playerNum == "3")
+                {
+
+                 // prompt names
+                 writeLine("What're your names");
+
+                 writeLine("Player 1: ");
+                 String p1 = getInput();
+
+                 writeLine("Player 2: ");
+                 String p2 = getInput();
+
+                 writeLine("Player 3: ");
+                 String p3 = getInput();
+
+                }
+
+                else if (playerNum == "4")
+                {
+
+                 // prompt names
+                 writeLine("What're your names");
+
+                 writeLine("Player 1: ");
+                 String p1 = getInput();
+
+                 writeLine("Player 2: ");
+                 String p2 = getInput();
+
+                 writeLine("Player 3: ");
+                 String p3 = getInput();
+
+                 writeLine("Player 4: ");
+                 String p4 = getInput();
+
+                }
+                    else;
+                
 
             // prompt user for category and level of difficulty
 
