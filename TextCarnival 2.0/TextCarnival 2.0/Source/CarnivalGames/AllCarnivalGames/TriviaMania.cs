3236e5ffb54f43825314b26585aac49675cdff3f
yasminjonson <yasminj2019@gmail.com>
2017-09-26 11:33:36 -0400

    game

---
+++ TextCarnival 2.0/TextCarnival 2.0/Source/CarnivalGames/AllCarnivalGames/TriviaMania.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCarnivalV2.Source.CarnivalGames.AllCarnivalGames
+{
+    class TriviaMania : CarnivalGame
+    {
+        public TriviaMania() : base()
+        {
+        }
+
+        public override string getName()
+        {return "Trivia Mania";}
+        
+        public override void play()
+        {
+            // code here
+
+            // print name and promp user for name and available prizes
+
+            // numer of players
+            Console.WriteLine("Hello and welcome to Trivia Mania!!!" +
+                "Please select number of players: ");
+
+
+
+            // prompt user for category and level of difficulty
+
+            // randomizer of those questions in each category and level
+
+            // list available prizes to choose one
+        }
+
+    }
+}
